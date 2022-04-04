2a814b950c03e648ce0a9f1eb8313bb644bff979
Meraviglioso <46748862+deveon8@users.noreply.github.com>
2022-04-04 09:35:01 +0700

    Add files via upload

---
+++ Stack_2.cpp
@@ -0,0 +1,81 @@
+#define MAX 100
+#include <iostream>
+using namespace std;
+struct Stack
+{
+    int a[MAX];
+    int top;
+};
+void CreateStack(Stack &s)
+{
+    s.top = -1;
+}
+bool isEmpty(Stack &s)
+{
+    if (s.top == -1)
+    {
+        return true;
+    }
+    else
+        return false;
+}
+bool isFull(Stack &s)
+{
+    if (s.top >= MAX - 1)
+    {
+        cout << "Stack is full.";
+        return true;
+    }
+    else
+        return false;
+}
+void Push(Stack &s, int item)
+{
+    if (isFull(s))
+    {
+        cout << "Stack is full.";
+        return;
+    }
+    else
+    {
+        s.a[++s.top] = item;
+    }
+}
+int Pop(Stack &s, int x = 0)
+{
+    if (isEmpty(s))
+        return 0;
+    else
+    {
+        x = s.a[s.top];
+        s.top--;
+        return x;
+    }
+}
+void Calculator(Stack &s, int coSo, int num)
+{
+    if (coSo == 2 || coSo == 8 || coSo == 16)
+        while (num != 0)
+        {
+            int x = num % coSo;
+            Push(s, x);
+            num /= coSo;
+        }
+    else
+        cout << "Co so khong hop le.";
+    
+    while (isEmpty(s)==false)
+    {
+        int x = Pop(s);
+            cout << x;
+}}
+
+int main(){
+    Stack temp;
+    CreateStack(temp);
+    int cs=2,s;
+    cout<<"Nhap so thap phan: ";
+    cin>>s;
+    Calculator(temp,cs,s);
+    return 0;
+}
